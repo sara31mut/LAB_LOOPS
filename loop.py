70eebb5a9bde47e84db6a703c9fbbbe108d8fecb
sara31mut <129232576+sara31mut@users.noreply.github.com>
2023-04-03 04:28:00 +0300

    lab solve

---
+++ loop.py
@@ -0,0 +1,23 @@
+#number 1
+one_to_ten = range(45, 210)
+
+for number in one_to_ten:
+    print("the number is: ", number)
+    if number == 201:
+        break
+
+
+   # number 2 
+result = 7*24
+print("the ruslt ", 7*24)
+
+result  (input("the answer:"))
+while result < 168:
+    print ( "You answered this Question correctly")
+else:
+    print ("Your Answer is wrong try again")
+
+
+
+ 
+
